e802d64c29bfa8d8d9c8678ca12b7e9295237909
Jean <jean.carrillo@udea.edu.co>
2023-04-12 13:02:47 -0500

    Error catch added to atm model

--- backend/src/controllers/atm.controller.js
+++ backend/src/controllers/atm.controller.js
@@ -19,24 +19,31 @@ export const getOneAtm = async(req, res) => {
 
 
 export const createAtm = async (req, res) => {
-    const {status, max_amount, min_amount, address} = req.body;
+    const {status, max_amount, min_amount, address, city_id} = req.body;
     if(
         max_amount == null||
         min_amount == null||
-        address == null
+        address == null ||
+        city_id == null
     ){
         res.status(400).json({
             message: 'Only propertie "status" can be empty.'
         });
     } else {
-        const newAtm = await atmModel.create({
-            code: uuid(),
-            status,
-            max_amount,
-            min_amount,
-            address
-        });
-        res.status(201).json(newAtm)
+        try {
+            const newAtm = await atmModel.create({
+                code: uuid(),
+                status,
+                max_amount,
+                min_amount,
+                address,
+                city_id
+            });
+            res.status(201).json(newAtm)
+        } catch (error) {
+            console.log(error.message);
+            res.status(400).json({message: error.message});
+        }
     }
 }
 
@@ -45,37 +52,46 @@ export const updateAtm = async (req, res) => {
     const {id} = req.params
     const atmToUpdate = await atmModel.findByPk(id)
     if(atmToUpdate){
-        const {status, max_amount, min_amount, address} = req.body;
-        atmToUpdate.status = status
-        atmToUpdate.max_amount = max_amount
-        atmToUpdate.min_amount = min_amount
-        atmToUpdate.address = address
-        await atmToUpdate.save();
-
-        res.status(200).json(atmToUpdate);
+        try {
+            const {status, max_amount, min_amount, address, city_id} = req.body;
+            atmToUpdate.status = status
+            atmToUpdate.max_amount = max_amount
+            atmToUpdate.min_amount = min_amount
+            atmToUpdate.address = address
+            atmToUpdate.city_id = city_id
+            await atmToUpdate.save();
+            console.log(atmToUpdate);
+            res.status(200).json(atmToUpdate);
+        } catch (error) {
+            console.log(error.message);
+            res.status(400).json({message: error.message});
+        }
     } else {
         res.status(404).json({
             message: `At the moment we have no ATM with id: ${id} to show. Please make sure that the provided id exists in the database.`
         });
     }
-    
-
 }
 
 
 export const deleteAtm = async (req, res) => {
     const {id} = req.params
     const atmToDelete = await atmModel.findByPk(id)
-
     if(atmToDelete){
-        await atmModel.destroy({
-            where: {
-                id
-            }
-        })
-        res.status(200).json({
-            message:`The ATM with id: ${id} was successfully deleted.`
-        });
+        try {
+            await atmModel.destroy({
+                where: {
+                    id
+                }
+            })
+            res.status(200).json({
+                message:`The ATM with id: ${id} was successfully deleted.`
+            });
+        } catch (error) {
+            console.log(error.message);
+            res.status(400).json({message: error.message});
+        }
+        
     } else {
         res.status(404).json({
             message: `The ATM with id: ${id} doesn't exist in the database.`
