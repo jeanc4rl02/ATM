1fe1b2730081dde3185d021f146a3d08fc4d7612
andersson980114 <anderson.980114@gmail.com>
2023-04-14 13:16:51 -0500

    validation

--- backend/src/models/city.model.js
+++ backend/src/models/city.model.js
@@ -4,7 +4,12 @@ import sequelize from '../database/atm.database.js';
 import { DataTypes } from 'sequelize';
 
 const cityModel = sequelize.define('city', { 
-        name: {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true 
+    },
+    name: {
             type: DataTypes.STRING, 
             allowNull: false,
             unique: true,
